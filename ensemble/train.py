79e33a0fd2bb59f55ca9a9838ed2f0f06e7edc5e
Ryan Holmdahl <ryanlh@stanford.edu>
2018-11-24 21:18:55 -1000

    Update train.py

--- ensemble/train.py
+++ ensemble/train.py
@@ -50,31 +50,19 @@ def _load_submodules(data_manager):
 
 def _train(data_manager, model):
     bar = Bar('Processing', max=100*len(data_manager.train_dbids)/64)
-    # train_losses = AverageMeter()
-    # dev_losses = AverageMeter()
-    # p_micro = AverageMeter()
-    # r_micro = AverageMeter()
-    # f_micro = AverageMeter()
-    # p_macro = AverageMeter()
-    # r_macro = AverageMeter()
-    # f_macro = AverageMeter()
-    # s_macro = AverageMeter()
-    # mAP_micro = AverageMeter()
-    # mAP_macro = AverageMeter()
-    # acc = AverageMeter()
-    dev_loss = 0.
-    train_loss = 0.
-    batch_p_micro = 0.
-    batch_r_micro = 0.
-    batch_f_micro = 0.
-    batch_s_micro = 0.
-    batch_p_macro = 0.
-    batch_r_macro = 0.
-    batch_f_macro = 0.
-    batch_s_macro = 0.
-    batch_mAP_micro = 0.
-    batch_mAP_macro = 0.
-    batch_acc = 0.
+    train_losses = AverageMeter()
+    dev_losses = AverageMeter()
+    p_micro = AverageMeter()
+    r_micro = AverageMeter()
+    f_micro = AverageMeter()
+    p_macro = AverageMeter()
+    r_macro = AverageMeter()
+    f_macro = AverageMeter()
+    s_macro = AverageMeter()
+    mAP_micro = AverageMeter()
+    mAP_macro = AverageMeter()
+    best_mAP_macro = 0.
+    acc = AverageMeter()
     total_positive_labels = (
         data_manager.train_labels.sum() + data_manager.dev_labels.sum() + data_manager.test_labels.sum()
     )
@@ -90,38 +78,31 @@ def _train(data_manager, model):
             train_inputs, targets = data_manager.sample_train_batch(64)
             logits = model.forward(train_inputs)
             loss = criterion(logits.reshape(-1), targets.reshape(-1))
-            # train_losses.update(loss.item(), 64)
-            train_loss = loss
+            train_losses.update(loss.item(), 64)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             bar.suffix = '({epoch}/{max_epochs}) | TLoss: {train_loss:.4f} | DLoss: {dev_loss:.4f} ' \
-                         '| Acc: {acc:.3f} | P: {p:.3f}| R: {r:.3f}| F: {f:.3f}| mAP mac: {mAP:.3f}|' \
+                         '| Acc: {acc:.3f} | P: {p:.3f}| R: {r:.3f}| F: {f:.3f}| mAP mac: {mAP:.3f} ' \
+                         '| best mAP {mAP_best:.3f}' \
                 .format(
                         epoch=epoch,
                         max_epochs=100,
-                        train_loss=train_loss,
-                        dev_loss=dev_loss,
-                        acc=batch_acc,
-                        p=batch_p_micro,
-                        r=batch_r_micro,
-                        f=batch_f_micro,
-                        mAP=batch_mAP_macro,
-                        # train_loss=train_losses.avg,
-                        # dev_loss=dev_losses.avg,
-                        # acc=acc.avg,
-                        # p=p_micro.avg,
-                        # r=r_micro.avg,
-                        # f=f_micro.avg,
-                        # mAP=mAP_macro.avg,
+                        train_loss=train_losses.avg,
+                        dev_loss=dev_losses.avg,
+                        acc=acc.avg,
+                        p=p_micro.avg,
+                        r=r_micro.avg,
+                        f=f_micro.avg,
+                        mAP=mAP_macro.avg,
+                        mAP_best=best_mAP_macro
                 )
             bar.next()
 
         dev_inputs, targets = data_manager.sample_dev_batch(121)
         logits = model.forward(dev_inputs)
         loss = criterion(logits.reshape(-1), targets.reshape(-1))
-        # dev_losses.update(loss.item(), 121)
-        dev_loss = loss
+        dev_losses.update(loss.item(), 121)
         (batch_p_micro,
          batch_r_micro,
          batch_f_micro,
@@ -133,16 +114,17 @@ def _train(data_manager, model):
          batch_mAP_micro,
          batch_mAP_macro,
          batch_acc) = compute_metrics(logit=logits, target=targets)
-        # p_macro.update(batch_p_macro, 121)
-        # p_micro.update(batch_p_micro, 121)
-        # r_macro.update(batch_r_macro, 121)
-        # r_micro.update(batch_r_micro, 121)
-        # f_macro.update(batch_f_macro, 121)
-        # f_micro.update(batch_f_micro, 121)
-        # s_macro.update(batch_s_macro, 121)
-        # mAP_micro.update(batch_mAP_micro, 121)
-        # mAP_macro.update(batch_mAP_macro, 121)
-        # acc.update(batch_acc, 121)
+        p_macro.update(batch_p_macro, 121)
+        p_micro.update(batch_p_micro, 121)
+        r_macro.update(batch_r_macro, 121)
+        r_micro.update(batch_r_micro, 121)
+        f_macro.update(batch_f_macro, 121)
+        f_micro.update(batch_f_micro, 121)
+        s_macro.update(batch_s_macro, 121)
+        mAP_micro.update(batch_mAP_micro, 121)
+        mAP_macro.update(batch_mAP_macro, 121)
+        acc.update(batch_acc, 121)
+        best_mAP_macro = max(best_mAP_macro, batch_mAP_macro)
 
     return model
 
