51f2a377ce8b47babc33bef22bb113b7cc624c4e
Ryan Holmdahl <ryanlh@stanford.edu>
2018-11-24 20:59:18 -1000

    Update protein_model.py

--- ensemble/protein/protein_model.py
+++ ensemble/protein/protein_model.py
@@ -54,11 +54,8 @@ class ProteinEmbeddingModel(nn.Module):
             )
         protein_rnn, _ = nn.utils.rnn.pad_packed_sequence(protein_rnn, padding_value=-np.infty)
         protein_rnn = protein_rnn.index_select(1, protein_unsort)  # [batch_size * proteins_per, max_protein_len]
-        print(protein_rnn.shape)
         protein_rnn = protein_rnn.reshape(self.config.max_len, batch_size, proteins_per, -1)
-        print(protein_rnn.shape)
         protein_maxpool = torch.max(protein_rnn, 0)[0]
         protein_maxpool = torch.max(protein_maxpool, 1)[0]  # [batch_size, embed_size]
-        print(protein_maxpool.shape)
 
         return protein_maxpool
