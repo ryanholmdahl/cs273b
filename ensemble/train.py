edd80aeb0dec6ef91fcb107effc7f71253ebdddd
Ryan Holmdahl <ryanlh@stanford.edu>
2018-11-26 16:49:18 -0800

    Update train.py

--- ensemble/train.py
+++ ensemble/train.py
@@ -14,8 +14,11 @@ from src.text_model_pipeline import compute_metrics
 from pytorch_classification.utils import AverageMeter, Bar
 
 
-# TODO: check against test
-# TODO: filter configs
+# TODO: smaller/shuffled dev set?
+# TODO: try removing submodules
+# TODO: run more epochs
+# TODO: train mAP
+# TODO: true ensemble (no shared differentiability)
 # TODO: load checkpoints
 
 
@@ -29,16 +32,16 @@ def _parse_args():
 
 def _load_data_manager(cuda):
     return EnsembleDataManager(cuda, 800, [
-        # (
-        #     ProteinDataManager, [
-        #         100,
-        #     ],
-        # ),
         (
-            TextDataManager, [
-                300, 50,
+            ProteinDataManager, [
+                100,
             ],
         ),
+        # (
+        #     TextDataManager, [
+        #         300, 50,
+        #     ],
+        # ),
         # (
         #     GoDataManager, []
         # ),
@@ -47,8 +50,8 @@ def _load_data_manager(cuda):
 
 def _load_submodules(data_manager):
     return (
-        # load_protein_models(data_manager.submodule_managers[0].vocab.n_words) #+
-        load_text_models(data_manager.submodule_managers[0].vocab.n_words) #+
+        load_protein_models(data_manager.submodule_managers[0].vocab.n_words) #+
+        # load_text_models(data_manager.submodule_managers[0].vocab.n_words) #+
         # load_go_models(data_manager.submodule_managers[2].num_terms)
     )
 
@@ -158,7 +161,7 @@ def _main():
     print('Data manager loaded.')
     submodules = _load_submodules(data_manager)
     data_manager.connect_to_model(submodules)
-    model = EnsembleModel(32 * 3, hiddens, 1121, submodules, 0.25)
+    model = EnsembleModel(32, hiddens, 1121, submodules, 0.25)
     if cuda:
         model = model.cuda()
     _train(data_manager, model)
