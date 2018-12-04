9cdede7c77acace5c94db9fa1f47598e2ba5576e
Ryan Holmdahl <ryanlh@stanford.edu>
2018-12-03 20:20:13 -0800

    Update train.py

--- ensemble/train.py
+++ ensemble/train.py
@@ -14,7 +14,6 @@ from src.text_model_pipeline import compute_metrics
 from pytorch_classification.utils import AverageMeter, Bar
 
 
-# TODO: retrain models on SIDER 4
 # TODO: change BCE pos weight to be a vector
 # TODO: freeze embeddings
 # TODO: try removing text submodules
@@ -34,11 +33,11 @@ def _parse_args():
 
 def _load_data_manager(cuda):
     return EnsembleDataManager(cuda, 800, [
-        (
-            ProteinDataManager, [
-                100,
-            ],
-        ),
+        # (
+        #     ProteinDataManager, [
+        #         100,
+        #     ],
+        # ),
         (
             TextDataManager, [
                 300, 50,
@@ -52,8 +51,8 @@ def _load_data_manager(cuda):
 
 def _load_submodules(data_manager):
     return (
-        load_protein_models(data_manager.submodule_managers[0].vocab.n_words) +
-        load_text_models(data_manager.submodule_managers[1].vocab.n_words) # +
+        # load_protein_models(data_manager.submodule_managers[0].vocab.n_words) +
+        load_text_models(data_manager.submodule_managers[0].vocab.n_words) # +
         # load_go_models(data_manager.submodule_managers[1].num_terms)
     )
 
@@ -163,7 +162,7 @@ def _main():
     print('Data manager loaded.')
     submodules = _load_submodules(data_manager)
     data_manager.connect_to_model(submodules)
-    model = EnsembleModel(32 * 4, hiddens, 5579, submodules, 0.25)
+    model = EnsembleModel(32 * 3, hiddens, 5579, submodules, 0.25)
     if cuda:
         model = model.cuda()
     _train(data_manager, model)
