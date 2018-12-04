4654a6fc0295c0b8623bdd702c7c238867885f1d
Ryan Holmdahl <ryanlh@stanford.edu>
2018-12-03 20:37:53 -0800

    Update constant.py

--- ensemble/constant.py
+++ ensemble/constant.py
@@ -12,8 +12,8 @@ else:
     SAVE_DIR = os.path.join(ROOT_PATH, 'save')
 GLOVE_DIR = os.path.join(DATA_DIR, 'glove')
 
-TRAIN_IDS = os.path.join('~', 'train_dbids_munoz.txt')
-TEST_IDS = os.path.join('~', 'test_dbids_munoz.txt')
+TRAIN_IDS = os.path.join('/home/ryanlh', 'train_dbids_munoz.txt')
+TEST_IDS = os.path.join('/home/ryanlh', 'test_dbids_munoz.txt')
 
 TRAIN_LABELS = os.path.join(DATA_DIR, 'train_labels_1.pkl')
 TEST_LABELS = os.path.join(DATA_DIR, 'test_labels_1.pkl')
