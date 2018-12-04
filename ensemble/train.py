a38b2c65e8fe32e2bb998931e8053fc06c2b15a8
Ryan Holmdahl <ryanlh@stanford.edu>
2018-12-04 14:30:07 -0800

    Update train.py

--- ensemble/train.py
+++ ensemble/train.py
@@ -35,7 +35,7 @@ def _parse_args():
     parser.add_argument('--single_pos_weight', action='store_true')
     parser.add_argument('--epochs', type=int)
     args = parser.parse_args()
-    return args.cuda, args.hiddens, args.dropout, args.embed_dims, args.embedders, args.use_pos_weights, \
+    return args.cuda, args.hiddens, args.dropout, args.embed_dims, args.embedders, args.use_pos_weight, \
            args.single_pos_weight, args.epochs
 
 
